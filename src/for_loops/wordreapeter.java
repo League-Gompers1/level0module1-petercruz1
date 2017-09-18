f8b7f4515cfb4a72acf90efd6df65b1253012ad8
petercruz1 <peter.cruz@jointheleague.org>
2017-09-18 14:51:27 -0700

    Add files via upload

---
+++ src/for_loops/wordreapeter.java
@@ -0,0 +1,16 @@
+package for_loops;
+
+import javax.swing.JOptionPane;
+
+public class wordreapeter {
+public static void main(String[] args) {
+	String amazing = JOptionPane.showInputDialog(null, "enter age");
+			
+	String cool = JOptionPane.showInputDialog(null, "enter letter");
+	 int g = Integer.parseInt(amazing);
+	for(int i = 0; i <= g;i++ ) {
+		System.out.println(cool);
+	}
+			
+}
+}
